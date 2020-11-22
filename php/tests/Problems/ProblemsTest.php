953a032cab3f1254c6cc19f326a2ec60c85afa67
Travis Weston <tweston2013@gmail.com>
2020-11-22 10:38:07 -0500

    Refactor problem tests

--- php/tests/Problems/ProblemsTest.php
+++ php/tests/Problems/ProblemsTest.php
@@ -4,14 +4,6 @@ use PHPUnit\Framework\TestCase;
 
 class ProblemsTest extends TestCase {
 
-    private $problems;
-
-    protected function setUp(): void {
-
-        $this->problems = new ProjectEuler\Problems\Two();
-    
-    }
-
     public function testMultiplesOf3And5() {
 
         $problem = new ProjectEuler\Problems\One();
@@ -21,16 +13,18 @@ class ProblemsTest extends TestCase {
 
     public function testFibonacci() {
 
-        $this->assertEquals(1, $this->problems->getFibonacciNumber(1));
-        $this->assertEquals(1, $this->problems->getFibonacciNumber(2));
-        $this->assertEquals(2, $this->problems->getFibonacciNumber(3));
-        $this->assertEquals(3, $this->problems->getFibonacciNumber(4));
+        $problem = new ProjectEuler\Problems\Two();
+        $this->assertEquals(1, $problem->getFibonacciNumber(1));
+        $this->assertEquals(1, $problem->getFibonacciNumber(2));
+        $this->assertEquals(2, $problem->getFibonacciNumber(3));
+        $this->assertEquals(3, $problem->getFibonacciNumber(4));
         
     }
 
     public function testFibonacciSum() {
 
-        $this->assertEquals(44, $this->problems->sumEvenFibonacciNumbersUnder(89));
+        $problem = new ProjectEuler\Problems\Two();
+        $this->assertEquals(44, $problem->sumEvenFibonacciNumbersUnder(89));
     
     }
 }
